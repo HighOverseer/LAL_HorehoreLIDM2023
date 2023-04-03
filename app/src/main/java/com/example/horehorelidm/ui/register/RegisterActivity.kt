6bb1a2a51b26969b914aeb83db8ed7264b4d9ce4
Akmal Rafi Nursyabani <akmalrafi.yourdev@gmail.com>
2023-04-04 06:37:55 +0700

    Register Screen

--- app/src/main/java/com/example/horehorelidm/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/horehorelidm/ui/register/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.example.horehorelidm.ui.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.horehorelidm.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
