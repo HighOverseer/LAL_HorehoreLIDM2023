88f2e02c40b5b6fb19381095ca4cdf538e0edab1
lawakobadu <73575111+afdalhalimamda@users.noreply.github.com>
2023-04-03 17:49:06 +0700

    login activity

--- app/src/main/java/com/example/horehorelidm/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/horehorelidm/ui/login/LoginActivity.kt
@@ -39,7 +39,7 @@ class LoginActivity : AppCompatActivity() {
             val loginState = it ?: return@Observer
 
             // disable login button unless both username / password is valid
-            login.isEnabled = loginState.isDataValid
+            login?.isEnabled = loginState.isDataValid
 
             if (loginState.usernameError != null) {
                 username?.error = getString(loginState.usernameError)
@@ -91,7 +91,7 @@ class LoginActivity : AppCompatActivity() {
                 false
             }
 
-            login.setOnClickListener {
+            login?.setOnClickListener {
                 loading.visibility = View.VISIBLE
                 loginViewModel.login(username?.text.toString(), password?.text.toString())
             }
